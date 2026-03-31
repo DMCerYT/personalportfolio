2b739ced9b41bd8fc57ad06ec61f194aad97ef9d
Emmanuel Munoz <72477837+DMCerYT@users.noreply.github.com>
2026-03-31 16:52:02 -0400

    finally updated logos in the contact page

--- src/data/siteAssets.js
+++ src/data/siteAssets.js
@@ -4,6 +4,23 @@ import focusFarmerLogo from '../assets/project-assets/focus-farmer/focus_farmer_
 import beanILogo from '../assets/project-assets/beani/beani_logo.svg';
 import moneyMentorLogo from '../assets/project-assets/money-mentor/Money_mentor_Logo.png';
 import capybaraIllustration from '../images/cute-cartoon-capybara-cap-sunglasses-600nw-2575177821.jpg.webp';
+// Social Media Assets
+import facebookPositive from '../assets/social-assets/facebook_positive.svg';
+import facebookNegative from '../assets/social-assets/facebook_negative.svg';
+import githubPositive from '../assets/social-assets/github_positive.svg';
+import githubNegative from '../assets/social-assets/github_negative.svg';
+import linkedInPositive from '../assets/social-assets/linkedin_positive.svg';
+import linkedInNegative from '../assets/social-assets/linkedin_negative.svg';
+import instagramPositive from '../assets/social-assets/instagram_positive.svg';
+import instagramNegative from '../assets/social-assets/instagram_negative.svg';
+import youtubePositive from '../assets/social-assets/youtube_positive.svg';
+import youtubeNegative from '../assets/social-assets/youtube_negative.svg';
+import googlePositive from '../assets/social-assets/google_positive.svg';
+import googleNegative from '../assets/social-assets/google_negative.svg';
+import twitterPositive from '../assets/social-assets/twitter_positive.svg';
+import twitterNegative from '../assets/social-assets/twitter_negative.svg';
+
+// Individual Project Assets
 
 export const siteAssets = {
   avatarLogo,
@@ -12,4 +29,20 @@ export const siteAssets = {
   focusFarmerLogo,
   beanILogo,
   moneyMentorLogo,
+  // Social Media Positive Assets
+  facebookPositive,
+  githubPositive,
+  linkedInPositive,
+  instagramPositive,
+  youtubePositive,
+  googlePositive,
+  twitterPositive,
+  // Social Media Negative Assets
+  facebookNegative,
+  githubNegative,
+  linkedInNegative,
+  instagramNegative,
+  youtubeNegative,
+  googleNegative,
+  twitterNegative
 };
